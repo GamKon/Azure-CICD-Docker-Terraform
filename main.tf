b2c06414ed5bfb0002c18d1b4efd82372536b8ed
GamKon <gamkon61@gmail.com>
2021-10-12 03:49:29 -0400

    TF provision instance with pulled Docker

--- main.tf
+++ main.tf
@@ -11,7 +11,36 @@ terraform {
     }
 }
 
+variable "imagebuild" {
+  type        = string
+  default = "latest"
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "gk-resource-froup" {
   name = "weather-rg"
   location = "Canada Central"
+}
+
+resource "azurerm_container_group" "gk-container-group" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.gk-resource-froup.location
+  resource_group_name       = azurerm_resource_group.gk-resource-froup.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "binarythistlewa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "gamkon61/gamkon-repo:${var.imagebuild}"
+//      image           = "gamkon61/gamkon-repo"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
 }
